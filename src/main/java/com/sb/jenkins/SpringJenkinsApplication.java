169e57723fea1c9c01deff32fd2144352334671c
pranay Dutta <pranaykumardutta@gmail.com>
2018-12-26 13:38:13 +0530

    commit first time

---
+++ src/main/java/com/sb/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,27 @@
+package com.sb.jenkins;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger log=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init()
+	{
+		log.info("Application strated..");
+	}
+
+	public static void main(String[] args) {
+		log.info("Application executed..");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
+
